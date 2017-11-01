a51d187ae700d62d317b9b8848b31ed9e17eaade
varxt <32694352+varxt@users.noreply.github.com>
2017-11-01 23:56:04 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const p = require('path');
+module.exports = {
+    entry:'./src/app.js',
+    output:{
+        filename: './bba.js',
+        path: p.resolve(__dirname, 'dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/.js$/,
+                use:['babel-loader'],
+            }
+        ]
+    }
+}
